8e4ba7e7d40ca3643f8527110f9e37d1487947e0
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-06-20 00:00:14 -0400

    added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 /* eslint-disable prettier/prettier */
 import { ThemeProvider } from "styled-components";
-
+import { BrowserRouter } from "react-router-dom";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Router } from "./router";
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variante="primary" />
-      <Button variante="secondary" />
-      <Button variante="danger" />
-      <Button variante="success" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
