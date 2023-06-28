a866dbcb8ec8b37663ce4f713d31958abe411a47
Gabriel-Leal <gabriel.leal.s.oliveira@gmail.com>
2023-06-27 23:19:58 -0400

    added countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,10 +8,12 @@ import {
   TaskInput,
   MinutesAmountInput,
 } from './styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { differenceInSeconds } from 'date-fns'
+
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'What is the task?'),
   minutesAmount: zod.number().min(5).max(60, 'The value must be up to 60'),
@@ -28,6 +30,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -52,11 +55,13 @@ export function Home() {
   })
 
   const id = String(new Date().getTime())
+
   function handleCreateNewCycle(data: NewCycleFormData) {
     const newCycle: Cycle = {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
@@ -66,6 +71,15 @@ export function Home() {
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate),
+        )
+      }, 1000)
+    }
+  }, [activeCycle])
   // console.log(activeCycle);
 
   // console.log(formState.errors);
